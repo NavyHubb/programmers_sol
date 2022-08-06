e35b4da2f4b1b7106982636aeef2edd502207ffc
Navy <101810007+NavyHubb@users.noreply.github.com>
2022-08-06 15:09:57 +0900

    Create test03.java

---
+++ test03.java
@@ -0,0 +1,26 @@
+package prac;
+
+import java.util.Scanner;
+
+public class test03 {
+
+	public static void main(String[] args) {
+		
+		Scanner ip = new Scanner(System.in); 
+		String a = ip.next();
+				
+		System.out.println(solution(a));
+		
+
+	}
+	
+    public static String solution(String s) {
+    	
+      //입력된 문자열의 길이가 홀수이든 짝수이든 통용가능
+    	String answer = s.substring( (s.length() - 1) / 2, s.length()/2 + 1); //length()는 몫(정수)만 return
+        return answer;
+        
+    }
+
+
+}
