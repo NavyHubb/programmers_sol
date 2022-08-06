e984caa8e3a9727b4c13fdfae9d813a131927619
Navy <101810007+NavyHubb@users.noreply.github.com>
2022-08-06 13:28:39 +0900

    Update test01.java

--- test01.java
+++ test01.java
@@ -10,7 +10,7 @@ public class test01 {
         
         for (int i = 0; i < b; i++) {
         	for(int j = 0; j < a; j++) {
-                System.out.print("*");  ##줄바꿈 없이 같은 줄에 연속적으로 출력해야 하므로 println()이 아닌 print() 사용 
+                System.out.print("*");  //줄바꿈 없이 같은 줄에 연속적으로 출력해야 하므로 println()이 아닌 print() 사용 
         	} 
         	System.out.println();
         }
