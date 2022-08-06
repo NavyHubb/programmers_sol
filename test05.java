f16be8f42adee69a2ee33b51b0f2d982532465c4
Navy <101810007+NavyHubb@users.noreply.github.com>
2022-08-06 15:24:10 +0900

    Create test05.java

---
+++ test05.java
@@ -0,0 +1,24 @@
+package prac;
+
+import java.util.Scanner;
+//title - 문자열을 정수로 바꾸기
+public class test05 {
+
+	public static void main(String[] args) {
+		
+		Scanner ip = new Scanner(System.in);
+		String a = ip.next(); //next()는 scanner에서 문자열을 공백을 기준으로 입력 및 리턴하는 method
+				
+		System.out.println(solution(a));
+		
+		ip.close();
+
+	}
+	
+	public static int solution(String s) {
+        
+		int answer = Integer.parseInt(s); //문자열을 정수로 변환하는 method
+	
+        return answer;
+    }
+}
