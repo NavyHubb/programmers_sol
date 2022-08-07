b3e86a6e1bc08ebf4e1535ef3e0203f7763e9083
Navy <101810007+NavyHubb@users.noreply.github.com>
2022-08-07 19:06:57 +0900

    Create test13.java

---
+++ test13.java
@@ -0,0 +1,20 @@
+package prac;
+//title - 2016년
+import java.time.*;
+
+public class test13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		int b = 24;
+		
+		System.out.println(solution(a, b));
+	}
+	
+	public static String solution(int a, int b) {
+        //LocaDate - java.time 패키지의 날짜를 나타내는 클래스
+        return LocalDate.of(2016, a, b).getDayOfWeek().toString().substring(0, 3);
+    }
+
+}
