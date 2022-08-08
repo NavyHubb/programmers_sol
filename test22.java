91d6b1957796bd919e9177e7720522a5000d067b
Navy <101810007+NavyHubb@users.noreply.github.com>
2022-08-08 14:36:34 +0900

    Create test22.java

---
+++ test22.java
@@ -0,0 +1,21 @@
+package prac;
+//title - 자릿수 더하
+public class test22 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int t = 123;
+		
+		System.out.println(solution(t));
+	}
+	
+	public static int solution(int n) {
+        int answer = 0;
+        
+        while(n != 0) {
+        	answer += n % 10; //10으로 나눈 나머지
+        	n /= 10;
+        }
+        return answer;
+    }
+}
