6cd366123e77aa2eb307830483fe1482f15cdd32
NavyHubb <sngjae972@gmail.com>
2022-08-11 15:04:53 +0900

    solution2 추가

--- test17.java
+++ test17.java
@@ -10,19 +10,32 @@ public class test17 {
 	}
 	
 	public static boolean solution(String s) {
-		
-		if(s.length() != 4 && s.length() != 6) 
+
+		if(s.length() != 4 && s.length() != 6)
 			return false;
-		
+
 		for(int i = 0; i < s.length(); i++) {
-			//ASCII 코드에 따라 char도 숫자에 대입되어 대소구분을 할 수 있다. 
-			//0~9의 경우 ASCII 코드 상 알파벳보다 작은 수로 표현된다. 
+			//ASCII 코드에 따라 char도 숫자에 대입되어 대소구분을 할 수 있다.
+			//0~9의 경우 ASCII 코드 상 알파벳보다 작은 수로 표현된다.
 			//단, ASCII 코드 상에는 알파벳과 숫자 외에도 특수문자 등 다른 문자들이 존재함에 유의
 			if(s.charAt(i) < '0' || s.charAt(i) > '9')
 				return false;
 		}
-		
+
 		return true;
     }
+
+	public static boolean solution2(String s) {
+
+		if(s.length() == 4 || s.length() == 6) {
+			//try-catch 구문: 예외가 발생하지 않을 경우와 발생할 경우 각각 수행할 코드 작성 가능
+			try {
+				int x = Integer.parseInt(s);
+				return true;
+			} catch (NumberFormatException e) {  //숫자 형식 오류
+				return false;
+			}
+		} else return false;
+	}
 
 }
